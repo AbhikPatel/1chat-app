d0d4428e450f81dff644d2bb63f94e2d6b951ce7
AbhikPatel <abhikpatel15@gmail.com>
2023-03-21 19:05:58 +0530

    feat: :art: Implemented login functionality
    Implemented login feature by creating form for the credentials, user email and password with validations. Setting the token in local storage

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { userAdaptor } from 'src/app/shared/adaptor/user.adaptor';
+import { environment } from 'src/environments/environment';
+import { HttpService } from '../http/http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  public userToken: any;
+  public api: string;
+
+  constructor(
+    private _http: HttpService,
+    private _adapt:userAdaptor
+  ) {
+    this.userToken = localStorage.getItem('token')
+    this.api = environment.baseURL
+  }
+
+  /**
+   * @name loginUser
+   * @param credentials 
+   * @returns http Post request 
+   * @description This method will call the post API for login to get user info
+   */
+  public loginUser(credentials: any): Observable<any> {
+    const url: string = this.api + 'users/log-in';
+    return this._http.httpPostRequest(url, credentials).pipe(
+      map((res) => {
+        localStorage.setItem('token', res.token)
+        return this._adapt.toResponse(res.data.doc)
+      })
+    )
+  }
 }
