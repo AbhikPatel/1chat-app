e9a755e621e734f035b4cde3da58fed1bf9b6ef2
m3hul7 <m3hul7@gmail.com>
2023-07-20 15:25:13 +0530

    feat: Implement notification prompt

--- src/app/shared/services/common.service.ts
+++ src/app/shared/services/common.service.ts
@@ -2,16 +2,12 @@ import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { Subject } from 'rxjs/internal/Subject';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
-import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn:'root'
 })
 
 export class CommonService {
-
-  public notificationData$: Observable<any>;
-  private notificationData: Subject<any>
 
   /** This Subject will store the user details */
   public user$: Subject<User>;
@@ -21,20 +17,12 @@ export class CommonService {
   public eodChatOpen: Subject<any>;
 
   constructor() {
-    this.notificationData = new Subject<any>();
-    this.notificationData$ = new Observable<any>();
-    this.notificationData$ = this.notificationData.asObservable();
     this.user$ = new Subject();
     this.closeModel = new Subject();
     this.statusDelete = new BehaviorSubject(false);
     this.submitEod = new BehaviorSubject(false);
     this.eodChatOpen = new Subject();
   }
-
-  
-  public notificationDataNext(val: any) {
-    this.notificationData.next(val);
-}
 
   /**
    * @name getUserId
