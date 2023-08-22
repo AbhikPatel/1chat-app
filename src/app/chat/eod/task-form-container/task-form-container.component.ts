ec1fa8d48e715c121349f642afa8490d8a9eccc6
Amresh Prasad <amresh1098@gmail.com>
2023-08-22 11:18:09 +0530

    fix : change folder strucutre

---
+++ src/app/chat/eod/task-form-container/task-form-container.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form-container',
+  templateUrl: './task-form-container.component.html'
+})
+export class TaskFormContainerComponent {
+
+}
---
+++ src/app/chat/eod/task-form-container/task-form-presentation/task-form-presentation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form-presentation',
+  templateUrl: './task-form-presentation.component.html'
+})
+export class TaskFormPresentationComponent {
+
+}
