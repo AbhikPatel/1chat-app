8553ce215097cb3f24085a7932cd40374b045477
Amresh Prasad <amresh1098@gmail.com>
2023-10-04 19:05:01 +0530

    feat:pagination set

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmationModelComponent } from './confirmation-model/confirmation-model.component';
 import { TruncatePipe } from './truncate.pipe';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-
-
 @NgModule({
   declarations: [
     ConfirmationModelComponent,
-    TruncatePipe
+    TruncatePipe,
+    
   ],
   imports: [
     CommonModule,
-
   ],
-  
-  
 })
 export class SharedModule { }
